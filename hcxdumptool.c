038130a48f094c963fabeb7c90c7e3b115781733
Mike <ZeroBeat@gmx.de>
2024-12-26 14:55:16 +0100

    removed debug code

--- hcxdumptool.c
+++ hcxdumptool.c
@@ -4077,11 +4077,13 @@ if(setsockopt(fd_socket_rx, SOL_PACKET, PACKET_IGNORE_OUTGOING, &enable, sizeof(
 if(bpf.len > 0)
 	{
 	if(setsockopt(fd_socket_rx, SOL_SOCKET, SO_ATTACH_FILTER, &bpf, sizeof(bpf)) < 0)
-	fprintf(stderr, "failed to attach BPF (SO_ATTACH_FILTER): %s\n", strerror(errno));
-	#ifdef HCXDEBUG
-	fprintf(fh_debug, "SO_ATTACH_FILTER failed: %s\n", strerror(errno));
-	#endif
-	return false;
+		{
+		fprintf(stderr, "failed to attach BPF (SO_ATTACH_FILTER): %s\n", strerror(errno));
+		#ifdef HCXDEBUG
+		fprintf(fh_debug, "SO_ATTACH_FILTER failed: %s\n", strerror(errno));
+		#endif
+		return false;
+		}
 	}
 memset(&saddr, 0, sizeof(saddr));
 saddr.sll_family = PF_PACKET;
