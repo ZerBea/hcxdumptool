bcc256b84403f5d6a8f7c530b4c64fdb18ad7cad
= <=>
2026-01-29 22:39:34 +0100

    added new option to rcascan to show only APs in range - see -h and changelog

--- hcxdumptool.c
+++ hcxdumptool.c
@@ -605,6 +605,29 @@ else if(rds == 3)
 			}
 		}
 	}
+else if(rds == 4)
+	{
+	qsort(aplist, APLIST_MAX, APLIST_SIZE, sort_aplist_by_tsresponse);
+	for(i = 0; i < APLIST_MAX - 1; i++)
+		{
+		if((aplist + i)->tsakt == 0) break;
+		if((scanlist + scanlistindex)->channel == (aplist + i)->apdata->channel)
+			{
+			tvlast = (aplist +i)->tsakt / 1000000000ULL;
+			strftime(timestring, TIMESTRING_LEN, "%H:%M:%S", localtime(&tvlast));
+			if((aplist + i)->apdata->tsresponse > 0)
+				{
+				tvlast = (aplist + i)->apdata->tsresponse / 1000000000ULL;
+				strftime(timestringresponse, TIMESTRING_LEN, "%H:%M:%S", localtime(&tvlast));
+				fprintf(stdout, "%3u|%s|%s|%4d|%02x%02x%02x%02x%02x%02x|%.*s\n",
+				(aplist + i)->apdata->channel, timestring, timestringresponse, (s8)(aplist + i)->apdata->rtrssi,
+				(aplist + i)->apdata->maca[00], (aplist + i)->apdata->maca[01], (aplist + i)->apdata->maca[02],
+				(aplist + i)->apdata->maca[03],	(aplist + i)->apdata->maca[04], (aplist + i)->apdata->maca[05],
+				(aplist + i)->apdata->essidlen, (aplist + i)->apdata->essid);
+				}
+			}
+		}
+	}
 qsort(aplist, APLIST_MAX, APLIST_SIZE, sort_aplist_by_tsakt);
 return;
 }
@@ -5349,6 +5372,7 @@ fprintf(stdout, "--ftc            : enable fake time clock\n"
 	"                     1 = show APs on current channel sorted by PROBERESPONSE timestamp\n"
 	"                     2 = show APs on current channel sorted by RSSI\n"
 	"                     3 = show APs of all channels sorted by RSSI\n"
+	"                     4 = show only APs in range on current channel sorted by PROBERESPONSE timestamp\n"
 	"--rdt            : disable TIOCGWINSZ for real time displays\n"
 	"--rcascan=<mode> : radio channel assessment scan\n"
 	"                    (a)ctive = active scan (transmit undirected PROBEREQUEST frames)\n"
